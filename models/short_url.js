82f7b0de190af7145966ce822592ca68c7f52130
Andrew Doppler <andrew.doppler@ombud.com>
2014-09-10 13:39:38 -0600

    Adding length constraint

--- models/short_url.js
+++ models/short_url.js
@@ -1,7 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
   var ShortUrl = sequelize.define('ShortUrl', {
-    url: DataTypes.STRING,
-    hits: {type: DataTypes.INTEGER, defaultValue: 0}
+    url: {
+      type: DataTypes.STRING(32)
+    },
+    hits: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
   }, {
     classMethods: {
 
