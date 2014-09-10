4a48fb321be4c01713a30899c2d4294bbd63d8e1
Andrew Doppler <andrew.doppler@ombud.com>
2014-09-10 14:33:28 -0600

    Removing old stub code

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = module.exports = express();
-
-var shrt = require('./shrt');
-
-var shrts = [];
 var db = require('./models');
+
 var initializeRoutes = require('./routes');
 
 app.set('port', process.env.PORT || 3000);
@@ -16,26 +13,25 @@ app.get('/', function(req, res, next) {
   res.render('index');
 });
 
-function initDb(callback) {
-  db
-    .sequelize
-    .sync({ force: true })
-    .complete(function(err) {
+var resetDatabaseOnStart = false;
 
+function initDb(callback) {
+  db.sequelize
+    .sync({ force: resetDatabaseOnStart })
+    .complete(function(err) {
       if (err) {
         callback(err[0]);
       } else {
         callback();
       }
-    })
+    });
 }
 
 function initServer(app) {
-  initDb(function(error){
-    if(error)
+  initDb(function(error) {
+    if (error) {
       throw error
-    else {
-      // Start Server
+    } else {
       app.listen(app.get('port'),function() {
         console.log('shrtn.ly listening on port ' + app.get('port'));
       });
