3cdf97220eb6fae7d7f976df261cdfda75180643
Andrew Doppler <andrew.doppler@ombud.com>
2014-09-10 13:39:19 -0600

    Saving and tracking urls

--- app.js
+++ app.js
@@ -11,41 +11,11 @@ var initializeRoutes = require('./routes');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
+
 app.get('/', function(req, res, next) {
   res.render('index');
 });
 
-app.get('/shrts', function(req, res, next) {
-  res.send(shrts);
-});
-
-app.post('/shrts', function(req, res, next) {
-  var data = req.body || {};
-  var hash_code = shrt.getCode(data.url);
-  var result = _.find(shrts, function(s) { return s.hash_code == hash_code });
-
-  if (!result) {
-    result = shrt.create(data.url);
-    shrts.push(result);
-  };
-
-  if (result && result.hash_code) {
-    res.send(result);
-  } else {
-    response.send(400);
-  };
-});
-
-app.get('/r/:hash_code', function(req, res, next)  {
-  var hash_code = req.param('hash_code');
-  var shrt = _.find(shrts, function(s) { return s.hash_code == hash_code });
-  if (shrt) {
-    shrt.clicks++;
-    res.redirect(shrt.url);
-  } else {
-    res.redirect('/');
-  }
 function initDb(callback) {
   db
     .sequelize
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Hashids = require("hashids");
 var db = require('../models');
+var _ = require('lodash');
 var ShortUrl = db.ShortUrl;
 var salt = "Shrt.ly Salt String"
-var hasher = new Hashids(salt, 8, "0123456789abcdef/");
+var hasher = new Hashids(salt, 8, "bcdfghjklmnpqrstvwxyz❀❃❆❇❊❋%/⌿#⌗");
 
 
 module.exports = function(app) {
@@ -12,13 +13,38 @@ module.exports = function(app) {
   // Middleware config
   app.use(bodyParser.json());
 
-  app.get('/:hash_code', function(req,res,next){
+  app.get(api_endpoint + '/shrts', function(req, res, next) {
+    ShortUrl.findAll().success(function(results) {
+      var list = _.map(results, function(s) {
+        var values = s.dataValues;
+        values.hash_code = hasher.encode(s.id);
+        return values;
+      });
+      res.send(list);
+    });
+  });
+
+  app.post(api_endpoint + '/shrts', function(req, res, next) {
+    var url = req.body.url;
+    ShortUrl.build({url:url}).save().complete(function(err,short) {
+      if(!err) {
+        var id = short.id;
+        res.json({url: url, hash_code: hasher.encode(id), hits: short.hits });
+      } else {
+        next(err);
+      }
+    });
+  });
+
+  app.get('/r/:hash_code', function(req,res,next){
     var hash_code = req.param('hash_code');
     var id = hasher.decode(hash_code);
     if(id.length>0) {
       ShortUrl.find(id[0]).success(function(shortUrl) {
-        if(shortUrl) {
-          res.send({"url":shortUrl.url});
+        if (shortUrl) {
+          shortUrl.hits++;
+          shortUrl.save();
+          res.redirect(shortUrl.url);
         } else {
           res.status(404).send({"message":"URL does not exist"});
         }
@@ -26,18 +52,6 @@ module.exports = function(app) {
     } else {
       res.status(404).send({"message":"URL does not exist"});
     }
-  });
-
-  app.post(api_endpoint + '/shorten', function(req,res,next){
-    var url = req.body.url;
-    ShortUrl.build({url:url}).save().complete(function(err,short) {
-      if(!err) {
-        var id = short.id;
-        res.json({url: url, short_url: hasher.encode(id)});
-      } else {
-        next(err);
-      }
-    });
   });
 
   app.all(api_endpoint + '*',function(req,res){
